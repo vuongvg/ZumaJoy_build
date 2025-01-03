86af6b433f8dfb29675f2e317e7f9f6af188758a
vuong.vu <vuongcdt@gmail.com>
2025-01-03 11:00:00 +0700

    9

---
+++ src/settings.c2812.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/game.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d17e2",main:"a7e79"}};
